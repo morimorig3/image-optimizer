a15ae47ee9743e9afc460faf9bf18ec9e011b15e
morimorig3 <mori.nto@outlook.jp>
2024-09-21 11:01:49 +0900

    ファイルタイプチェック

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,8 +16,9 @@ import (
 )
 
 const (
-	JPG = ".jpg"
-	PNG = ".png"
+	JPG  = ".jpg"
+	JPEG = ".jpeg"
+	PNG  = ".png"
 )
 
 func main() {
@@ -20,15 +26,15 @@ func main() {
 	args := flag.Args()
 	// 引数チェック
 	if len(args) < 1 {
-		fmt.Println("ファイル名を指定してください")
+		log.Println("ファイル名を指定してください")
 		os.Exit(1)
 	}
-	fileName := args[0]
-	TARGET_EXTENSIONS := []string{JPG, PNG}
-	ext := filepath.Ext(fileName)
+	inputFileName := args[0]
+	TARGET_EXTENSIONS := []string{JPG, JPEG, PNG}
+	ext := filepath.Ext(inputFileName)
 	// 拡張子有無チェック
 	if ext == "" || ext == "." {
-		fmt.Println("ファイル名に拡張子が存在しません")
+		log.Println("ファイル名に拡張子が存在しません")
 		os.Exit(1)
 	}
 	// ファイル名拡張子チェック
@@ -36,8 +42,28 @@ func main() {
 		return strings.EqualFold(ext, s)
 	})
 	if !isTargetExt {
-		fmt.Printf("拡張子 %s は対応しておりません\n", ext)
+		log.Printf("拡張子 %s は対応しておりません\n", ext)
 		os.Exit(1)
 	}
-	fmt.Println(fileName)
+
+	// ファイルを開く
+	f, err := os.Open(inputFileName)
+	if err != nil {
+		log.Println("ファイルを開くことができませんでした")
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// ファイルタイプチェック
+	buf := make([]byte, 512)
+	if _, err := io.ReadAtLeast(f, buf, 512); err != nil {
+		log.Println("ファイルを開くことができませんでした")
+		os.Exit(1)
+	}
+	mimeType := http.DetectContentType(buf)
+	fmt.Println(mimeType)
+	if mimeType != "image/jpeg" && mimeType != "image/png" {
+		log.Println("jpgまたはpngファイルを指定してください")
+		os.Exit(1)
+	}
 }

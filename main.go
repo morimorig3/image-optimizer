b13631797114f16e04a064a50e92a7671abde0be
morimorig3 <mori.nto@outlook.jp>
2024-09-21 10:02:37 +0900

    引数の文字列を受け取る

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("init")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("ファイル名を指定してください。")
+		os.Exit(1)
+	}
+	fileName := args[0]
+	fmt.Println(fileName)
 }

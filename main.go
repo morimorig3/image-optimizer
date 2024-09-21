b2730187161aed2253c7973eff2c1857685f38b2
morimorig3 <mori.nto@outlook.jp>
2024-09-21 10:06:44 +0900

    拡張子チェック

--- main.go
+++ main.go
@@ -4,15 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
+	// 引数チェック	
 	if len(args) < 1 {
 		fmt.Println("ファイル名を指定してください。")
 		os.Exit(1)
 	}
 	fileName := args[0]
+	ext := filepath.Ext(fileName)
+	// ファイル名拡張子チェック
+	if ext != ".jpg" {
+		fmt.Printf("拡張子%s は対応しておりません。\n", ext)
+		os.Exit(1)
+	}
 	fmt.Println(fileName)
 }

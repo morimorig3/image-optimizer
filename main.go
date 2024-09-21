4c7501aa16123e3e5ee4790000f1d71b878dfad3
morimorig3 <mori.nto@outlook.jp>
2024-09-21 10:40:51 +0900

    拡張子の有無チェック

--- main.go
+++ main.go
@@ -20,18 +20,23 @@ func main() {
 	args := flag.Args()
 	// 引数チェック
 	if len(args) < 1 {
-		fmt.Println("ファイル名を指定してください。")
+		fmt.Println("ファイル名を指定してください")
 		os.Exit(1)
 	}
 	fileName := args[0]
 	TARGET_EXTENSIONS := []string{JPG, PNG}
 	ext := filepath.Ext(fileName)
+	// 拡張子有無チェック
+	if ext == "" || ext == "." {
+		fmt.Println("ファイル名に拡張子が存在しません")
+		os.Exit(1)
+	}
 	// ファイル名拡張子チェック
 	isTargetExt := slices.ContainsFunc(TARGET_EXTENSIONS, func(s string) bool {
 		return strings.EqualFold(ext, s)
 	})
 	if !isTargetExt {
-		fmt.Printf("拡張子 %s は対応しておりません。\n", ext)
+		fmt.Printf("拡張子 %s は対応しておりません\n", ext)
 		os.Exit(1)
 	}
 	fmt.Println(fileName)

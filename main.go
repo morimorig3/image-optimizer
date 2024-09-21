5fabc38c28a29c2592a157a4727eb38e220a7854
morimorig3 <mori.nto@outlook.jp>
2024-09-21 10:38:38 +0900

    複数の拡張子チェックに対応する

--- main.go
+++ main.go
@@ -5,21 +5,33 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+const (
+	JPG = ".jpg"
+	PNG = ".png"
 )
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	// 引数チェック	
+	// 引数チェック
 	if len(args) < 1 {
 		fmt.Println("ファイル名を指定してください。")
 		os.Exit(1)
 	}
 	fileName := args[0]
+	TARGET_EXTENSIONS := []string{JPG, PNG}
 	ext := filepath.Ext(fileName)
 	// ファイル名拡張子チェック
-	if ext != ".jpg" {
-		fmt.Printf("拡張子%s は対応しておりません。\n", ext)
+	isTargetExt := slices.ContainsFunc(TARGET_EXTENSIONS, func(s string) bool {
+		return strings.EqualFold(ext, s)
+	})
+	if !isTargetExt {
+		fmt.Printf("拡張子 %s は対応しておりません。\n", ext)
 		os.Exit(1)
 	}
 	fmt.Println(fileName)
